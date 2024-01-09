95baba1149e2bb133df5df48b4662cbe535342fc
Nakkina Bhargav <nakkinabhargav@Nakkinas-Air.bbrouter>
2024-01-10 00:00:15 +0530

    portfolio first version

---
+++ src/models/index.js
@@ -0,0 +1,13 @@
+import Sky from "./Sky";
+import Bird from './Bird'
+import Plane from "./Plane";
+import Island from "./Island";
+import Fox from "./Fox"
+
+export {
+    Sky,
+    Bird,
+    Plane,
+    Island,
+    Fox,
+}
